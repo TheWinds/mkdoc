2b2f8bfd9b73a0490982697b161eba6cde94762e
thewinds <wind_ns@163.com>
2019-08-18 23:18:26 +0800

    scan doc from graphql schema

--- scan.go
+++ scan.go
@@ -55,6 +55,7 @@ func scanGraphQLAPIDocInfo(pkg string) ([]*API, error) {
 	subDirs := getSubDirs(rootDir)
 	count := 0
 	fileImports := map[string]map[string]string{}
+
 	for _, dir := range subDirs {
 		f := token.NewFileSet()
 		pkgs, err := parser.ParseDir(f, dir, nil, parser.ParseComments)
@@ -78,7 +79,7 @@ func scanGraphQLAPIDocInfo(pkg string) ([]*API, error) {
 					fImportFilesMap[importName] = importPath
 					fmt.Println("  ", importName, importPath)
 				}
-				fileImports[file.Name.Name] = fImportFilesMap
+				fileImports[fileName] = fImportFilesMap
 			}
 		}
 
@@ -89,14 +90,25 @@ func scanGraphQLAPIDocInfo(pkg string) ([]*API, error) {
 					if strings.HasPrefix(name, "\"") && strings.HasSuffix(name, "\"") {
 						value := readCode(f, kvExpr.Value)
 						if strings.HasPrefix(value, "&graphql.Field{") {
-							fmt.Println("key:", name)
-							fmt.Println("file:", f.Position(kvExpr.Pos()).Filename)
-							fmt.Println(value)
+							fileName := f.Position(kvExpr.Pos()).Filename
+							//fmt.Println(value)
+							if !strings.Contains(value, "@apidoc") {
+								return true
+							}
+							fmt.Println("file:", fileName)
 							fmt.Println()
 							count++
-						}
+							//ast.Print(f,kvExpr)
 
+							s := &GraphQLResolveSource{
+								NodeAST: kvExpr,
+								Code:    value,
+								Imports: fileImports[fileName],
+							}
+							s.GetAPI()
+						}
 					}
+					//return false
 				}
 				return true
 			})
@@ -107,28 +119,146 @@ func scanGraphQLAPIDocInfo(pkg string) ([]*API, error) {
 	return nil, nil
 }
 
-func parseDocDefFromGraphqlSrc(resolveName string, code string, imports map[string]string) (string, error) {
+func parseDocDefFromGraphqlSrc(nodeAST *ast.KeyValueExpr, code string, imports map[string]string) (string, error) {
 	graphQLResolveSource := &GraphQLResolveSource{
-		ResolveName: resolveName,
-		Code:        code,
-		Imports:     imports,
+		NodeAST: nodeAST,
+		Code:    code,
+		Imports: imports,
 	}
-	graphQLResolveSource.GetType()
+	graphQLResolveSource.GetAPI()
 	return "", nil
 }
 
 type GraphQLResolveSource struct {
-	ResolveName string
-	Code        string
-	Imports     map[string]string
+	NodeAST *ast.KeyValueExpr
+	Code    string
+	Imports map[string]string
 }
 
-func (g *GraphQLResolveSource) GetType() {
+func assertBasicLit(i interface{}) (*ast.BasicLit, bool) {
+	r, ok := i.(*ast.BasicLit)
+	return r, ok
+}
 
+func assertGraphQLFieldElts(i interface{}) ([]ast.Expr, bool) {
+	if unaryExpr, ok := i.(*ast.UnaryExpr); ok {
+		if compositeLit, ok := unaryExpr.X.(*ast.CompositeLit); ok {
+			return compositeLit.Elts, true
+		} else {
+			return nil, false
+		}
+	} else {
+		return nil, false
+	}
+}
+func (g *GraphQLResolveSource) GetAPI() (api *API, err error) {
+	nodeAPIName, ok := assertBasicLit(g.NodeAST.Key)
+	if !ok {
+		err = fmt.Errorf("not support:key is not string")
+		return
+	}
+	fmt.Println("API name:", nodeAPIName.Value)
+	api = new(API)
+	api.Name = nodeAPIName.Value
+	api.Type = "graphql"
+	api.Method = "query"
+	fieldElts, ok := assertGraphQLFieldElts(g.NodeAST.Value)
+	if !ok {
+		err = fmt.Errorf("not support:graphql api must define as a &graphql.Field")
+		return
+	}
+
+	for _, elt := range fieldElts {
+		switch elt.(type) {
+		case *ast.KeyValueExpr:
+			expr := elt.(*ast.KeyValueExpr)
+			keyName := expr.Key.(*ast.Ident).Name
+			switch keyName {
+			case "Type", "Args":
+				fmt.Println(keyName, ": ")
+				//  通过 从GoType 定义的参数类型
+				if callExpr, ok := expr.Value.(*ast.CallExpr); ok {
+					typeConvFuncName := callExpr.Fun.(*ast.SelectorExpr).Sel.Name
+					switch callExpr.Args[0].(type) {
+					case *ast.CompositeLit:
+						typeExpr := callExpr.Args[0].(*ast.CompositeLit).Type.(*ast.SelectorExpr)
+						packageName := typeExpr.X.(*ast.Ident).Name
+						typeName := typeExpr.Sel.Name
+						fmt.Println("- Fun:", typeConvFuncName)
+						fmt.Println("- PackageName:", packageName)
+						fmt.Println("- TypeName:", typeName)
+						isRepeated := strings.Contains(typeConvFuncName, "List")
+						typeLoc := &TypeLocation{
+							PackageName: g.Imports[packageName],
+							TypeName:    typeName,
+							IsRepeated:  isRepeated,
+						}
+						fmt.Println("- GoType:", typeLoc.String())
+						if keyName == "Type" {
+							api.outArgumentLoc = typeLoc
+						} else {
+							api.inArgumentLoc = typeLoc
+						}
+					case *ast.SelectorExpr:
+						//fmt.Println("- TypeName:", "NoName")
+						//typeExpr := callExpr.Args[0].(*ast.SelectorExpr)
+						//fmt.Println(typeExpr.X.(*ast.Ident).Name)
+
+					}
+
+				}
+				// 通过 FieldConfigArgument 定义的参数类型
+				if lit, ok := expr.Value.(*ast.CompositeLit); ok {
+					if litType, ok := lit.Type.(*ast.SelectorExpr); ok {
+						if litType.Sel.Name == "FieldConfigArgument" {
+							inObj := &Object{
+								ID: fmt.Sprintf("intype.graphql.%s", api.Name),
+							}
+							fields := make([]*ObjectField, 0)
+							for _, e := range lit.Elts {
+								if argKV, ok := e.(*ast.KeyValueExpr); ok {
+									fieldName := argKV.Key.(*ast.BasicLit).Value
+									fmt.Println("field name:", fieldName)
+									field := new(ObjectField)
+									field.Name = fieldName
+									field.JSONTag = fieldName
+									for _, fieldAttrElt := range argKV.Value.(*ast.UnaryExpr).X.(*ast.CompositeLit).Elts {
+										attrName := fieldAttrElt.(*ast.KeyValueExpr).Key.(*ast.Ident).Name
+										switch attrName {
+										case "Type":
+											field.Type = g.mapToGoType(fieldAttrElt.(*ast.KeyValueExpr).Value.(*ast.SelectorExpr).Sel.Name)
+											fmt.Println("field type:", field.Type)
+										case "Description":
+											field.Comment = fieldAttrElt.(*ast.KeyValueExpr).Value.(*ast.BasicLit).Value
+											fmt.Println("field desc:", field.Comment)
+										}
+									}
+									fields = append(fields, field)
+								}
+							}
+							inObj.Fields = fields
+							api.InArgument = inObj
+						}
+					}
+				}
+			default:
+			}
+		default:
+		}
+	}
+	err = api.Gen("corego/service/zhike-student/api")
+	if err != nil {
+		return
+	}
+	api.PrintMarkdown()
+
+	return
+	//g.NodeAST.
 	typeConvFuncNames := []string{
 		"NewGraphQLListTypeFromRPCType",
 		"NewGraphQLTypeFromRPCType",
 		"NewGraphQLArgsFromRPCType"}
+	typeConvFuncNames[0] = ""
 	lines := strings.Split(g.Code, "\n")
 	for _, line := range lines {
 		if strings.Contains(line, "Type") {
@@ -141,17 +271,26 @@ func (g *GraphQLResolveSource) GetType() {
 
 		}
 	}
+	return
+}
 
+func (g *GraphQLResolveSource) mapToGoType(graphQLType string) string {
+	m := map[string]string{
+		"Int":    "int64",
+		"String": "string",
+	}
+	return m[graphQLType]
 }
 
 func (g *GraphQLResolveSource) getGoType(codeLine string) string {
-	
+
 	if strings.Contains(codeLine, "Type") {
 
 	}
 	if strings.Contains(codeLine, "Arg") {
 
 	}
+	panic("")
 }
 
 func (g *GraphQLResolveSource) getFuncArg(s, funcName string) string {
