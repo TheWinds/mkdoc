e50f226129029be7fe4c988961ce709a92ba2c52
thewinds <wind_ns@163.com>
2019-08-21 00:16:01 +0800

    cmd: add flag and arg check

--- cmd/mkdoc.go
+++ cmd/mkdoc.go
@@ -3,7 +3,11 @@ package main
 import (
 	"docspace"
 	"docspace/scanners"
+	"fmt"
 	"gopkg.in/alecthomas/kingpin.v2"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
 var (
@@ -19,6 +23,8 @@ func init() {
 		new(scanners.CoregoGraphQLAPIScanner),
 		new(scanners.CoregoEchoAPIScanner),
 	}
+	scannersMap = map[string]docspace.APIScanner{}
+
 	for _, v := range scannerList {
 		scannersMap[v.GetName()] = v
 	}
@@ -26,7 +32,41 @@ func init() {
 
 func main() {
 	kingpin.Parse()
-	if scannersMap[*scannerName] == nil {
-
+	scanner := scannersMap[*scannerName]
+	if scanner == nil {
+		fmt.Printf("error: scanner \"%s\" is not found,you can choose scanner below :\n", *scannerName)
+		for name := range scannersMap {
+			fmt.Printf("    %s\n", name)
+		}
+		return
 	}
+	goPaths := getGOPaths()
+	pkgExist := false
+	for _, gopath := range goPaths {
+		if _, err := os.Stat(filepath.Join(gopath, "src", *pkg)); err == nil {
+			pkgExist = true
+		}
+	}
+	if !pkgExist {
+		fmt.Printf("error: package \"%s\" is not found in :\n", *pkg)
+		for _, gopath := range goPaths {
+			fmt.Println("  ", filepath.Join(gopath, "src", *pkg))
+		}
+	}
+	annotations, err := scanner.ScanAnnotations(*pkg)
+	if err != nil {
+		fmt.Printf("error: scan annotations %v\n", err)
+	}
+	for _, v := range annotations {
+		fmt.Println(v)
+	}
+
+}
+
+func getGOPaths() []string {
+	gopath := os.Getenv("GOPATH")
+	if strings.Contains(gopath, ":") {
+		return strings.Split(gopath, ":")
+	}
+	return []string{gopath}
 }
