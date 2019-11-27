f3db311ee7faf96f9732fa0f5f31f2d3fec123e2
thewinds <wind_ns@163.com>
2019-11-27 19:00:27 +0800

    feat: add config field

--- project.go
+++ project.go
@@ -1,6 +1,19 @@
 package docspace
 
 type Project struct {
-	BasePackage string
-	UseGOModule bool
+	Name         string    `yaml:"name"`
+	Description  string    `yaml:"desc"`
+	APIBaseURL   string    `yaml:"api_base_url"`  // https://api.xxx.com
+	BodyEncoder  string    `yaml:"body_encoder"`  // json,xml,form
+	CommonHeader []*Header `yaml:"common_header"` //
+	BasePackage  string    `yaml:"pkg"`           //
+	BaseType     string    `yaml:"base_type"`     // models.BaseType
+	UseGOModule  bool      `yaml:"use_go_mod"`
+	Scanner      []string  `yaml:"scanner"`
+}
+
+type Header struct {
+	Name    string `yaml:"name"`
+	Desc    string `yaml:"desc"`
+	Default string `yaml:"default"`
 }
