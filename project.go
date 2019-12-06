ae1046fcda126687481eb5a8b71d4f5b106a3e8d
thewinds <wind_ns@163.com>
2019-12-07 00:12:46 +0800

    refactor: mv create project func

--- project.go
+++ project.go
@@ -1,6 +1,12 @@
 package docspace
 
-import "sync"
+import (
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+	"strings"
+	"sync"
+)
 
 type Project struct {
 	Config     *Config
@@ -10,23 +16,24 @@ type Project struct {
 	ModulePath string
 }
 
-type Header struct {
-	Name    string `yaml:"name"`
-	Desc    string `yaml:"desc"`
-	Default string `yaml:"default"`
-}
+func NewProject(config *Config) (*Project, error) {
+	if err := config.Check(); err != nil {
+		return nil, err
+	}
+	project := &Project{Config: config}
+	if err := project.checkScanner(); err != nil {
+		return nil, err
+	}
 
-type Config struct {
-	Name         string    `yaml:"name"`
-	Description  string    `yaml:"desc"`
-	APIBaseURL   string    `yaml:"api_base_url"`  // https://api.xxx.com
-	BodyEncoder  string    `yaml:"body_encoder"`  // json,xml,form
-	CommonHeader []*Header `yaml:"common_header"` //
-	Package      string    `yaml:"pkg"`           //
-	BaseType     string    `yaml:"base_type"`     // models.BaseType
-	UseGOModule  bool      `yaml:"use_go_mod"`
-	Scanner      []string  `yaml:"scanner"`
-	Generator    []string  `yaml:"generator"`
+	if err := project.checkGenerator(); err != nil {
+		return nil, err
+	}
+	if config.UseGOModule {
+		if err := project.initGoModule(); err != nil {
+			return nil, err
+		}
+	}
+	return project, nil
 }
 
 var projectOnce sync.Once
@@ -43,4 +50,58 @@ func GetProject() *Project {
 		panic("_project is nil")
 	}
 	return _project
+}
+
+func (project *Project) checkScanner() error {
+	var okScanners []APIScanner
+	scanners := GetScanners()
+	if len(project.Config.Scanner) == 0 {
+		return fmt.Errorf("please use at least one scanner")
+	}
+
+	for _, name := range project.Config.Scanner {
+		if scanners[name] == nil {
+			sb := strings.Builder{}
+			sb.WriteString(fmt.Sprintf("scanner \"%s\" is not found,you can choose scanner below :\n", name))
+			for n := range scanners {
+				sb.WriteString(fmt.Sprintf("    %s\n", n))
+			}
+			return fmt.Errorf(sb.String())
+		}
+		okScanners = append(okScanners, scanners[name])
+	}
+	project.Scanners = okScanners
+	return nil
+}
+
+func (project *Project) checkGenerator() error {
+	var okGenerators []DocGenerator
+	generators := GetGenerators()
+	if len(project.Config.Generator) == 0 {
+		return fmt.Errorf("please use at least one generator")
+	}
+
+	for _, name := range project.Config.Generator {
+		if generators[name] == nil {
+			sb := strings.Builder{}
+			sb.WriteString(fmt.Sprintf("generator \"%s\" is not found,you can choose generator below :\n", name))
+			for n := range generators {
+				sb.WriteString(fmt.Sprintf("    %s\n", n))
+			}
+			return fmt.Errorf(sb.String())
+		}
+		okGenerators = append(okGenerators, generators[name])
+	}
+	project.Generators = okGenerators
+	return nil
+}
+
+func (project *Project) initGoModule() error {
+	data, err := ioutil.ReadFile(filepath.Join(project.Config.Package, "go.mod"))
+	if err != nil {
+		return err
+	}
+	project.ModulePkg = ModulePath(data)
+	project.ModulePath = FindGOModAbsPath(project.Config.Package)
+	return nil
 }
