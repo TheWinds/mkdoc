beccea2e94e74c3ce9b7e9cee6b501045f7fd343
thewinds <wind_ns@163.com>
2020-05-02 00:21:27 +0800

    chore: register gapi loader

--- cmd/mkdoc/plugin.go
+++ cmd/mkdoc/plugin.go
@@ -4,6 +4,7 @@ import (
 	_ "github.com/thewinds/mkdoc/generator/docsify"
 	_ "github.com/thewinds/mkdoc/generator/insomnia"
 	_ "github.com/thewinds/mkdoc/generator/markdown"
+	_ "github.com/thewinds/mkdoc/objloader/gapiloader"
 	_ "github.com/thewinds/mkdoc/objloader/goloader"
 	_ "github.com/thewinds/mkdoc/scanner/docdef"
 	_ "github.com/thewinds/mkdoc/scanner/gofunc"
