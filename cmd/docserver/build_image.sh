cc33c806c9fb5afe635c0d5bd5ab9b32df218d16
thewinds <wind_ns@163.com>
2020-05-16 20:48:46 +0800

    feat: build tag

--- cmd/docserver/build_image.sh
+++ cmd/docserver/build_image.sh
@@ -6,9 +6,9 @@ echo "building mkdoc"
 cd ../mkdoc && GOOS=linux go build -o mkdoc && mv mkdoc ../docserver
 cd "$BUILDDIR"
 echo "building docker image"
-docker build -t thewinds/mkdoc-server .
+docker build -t thewinds/mkdoc-server:zk .
 echo "push docker image"
-docker push thewinds/mkdoc-server
+docker push thewinds/mkdoc-server:zk
 echo "clean up"
 rm "$BUILDDIR/mkdoc"
 rm "$BUILDDIR/docserver"
